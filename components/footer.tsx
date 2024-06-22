292ab6ff45f0eb3cbbf1d9dc68ea354c4e68f6aa
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-06-22 17:47:42 +0200

    🏗️ Footer improvement with shameless self-promo.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
 import {Link} from "@nextui-org/react";
-import {useLocale, useTranslations} from "next-intl";
+import {useLocale, useNow, useTranslations} from "next-intl";
 import {FunctionComponent} from "react";
 
 export default (() => {
   const t = useTranslations();
   const locale = useLocale();
+  const now = useNow();
 
   const menuItems: MenuItems = {
     [t("navigation.menu.features.title")]: [
@@ -59,7 +60,8 @@ export default (() => {
           ))}
         </div>
         <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
+            © {now.getFullYear()} <Link href="/" color="foreground" size="sm" underline="hover">{t('footer.copyright')}</Link>. {t('footer.rightsReserved')}
           </span>
           <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
             <a href="#" className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
