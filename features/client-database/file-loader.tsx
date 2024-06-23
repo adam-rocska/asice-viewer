0f16c392ec70e3a560f48548a275228b03bc23e7
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-06-23 23:09:32 +0200

    🏗️ Yes, it is a lot better like this.

--- features/client-database/file-loader.tsx
+++ features/client-database/file-loader.tsx
@@ -1,8 +1,8 @@
 "use client";
-import {Button, ButtonProps} from "@nextui-org/react";
+import {Button, ButtonProps, Modal, ModalBody, ModalContent} from "@nextui-org/react";
 import clsx from "clsx";
 import {FunctionComponent, useRef} from "react";
-import Dropzone from "react-dropzone";
+import Dropzone, {DropzoneOptions} from "react-dropzone";
 import PaperPlus from "@/icons/paper-plus.svg";
 import {useTranslations} from "next-intl";
 import {createPortal} from "react-dom";
@@ -14,61 +14,53 @@ export default (p => {
   const showDropzone = useBooleanState(false);
   useEventListener('dragenter', showDropzone.set, body);
   const t = useTranslations();
+  const dropzoneProps: DropzoneOptions = {
+    onDragEnter: showDropzone.set,
+    onDrop: acceptedFiles => {
+      console.log(acceptedFiles);
+      showDropzone.unset();
+    },
+    /// TODO: double check: https://www.id.ee/en/article/bdoc2-1-new-estonian-digital-signature-standard-format/
+    accept: {'application/vnd.etsi.asic-e+zip': ['.asice', '.sce', '.bdoc']}
+  };
 
   return (
     <>
-      <Dropzone onDrop={acceptedFiles => {
-        console.log(acceptedFiles);
-        showDropzone.unset();
-      }}
-        accept={{
-          /// TODO: double check: https://www.id.ee/en/article/bdoc2-1-new-estonian-digital-signature-standard-format/
-          'application/vnd.etsi.asic-e+zip': ['.asice', '.sce', '.bdoc']
-        }}
-        onDragEnter={showDropzone.set}
-        onDragLeave={showDropzone.unset}
-      >
-        {({getRootProps, getInputProps}) => (
+      <Dropzone {...dropzoneProps}>
+        {({getRootProps, getInputProps, open}) => (
           <>
-            <Button {...p} />
-            {
-              showDropzone.isEnabled && createPortal(
-                (
-                  <div {...getRootProps()} className={clsx(
-                    'bg-slate-950/30 backdrop-blur-md',
-                    'fixed top-0 left-0 right-0 bottom-0 z-50 w-screen h-screen',
-                    'flex items-center justify-center',
-                  )}>
-                    <label className={clsx(
-                      "bg-slate-50 dark:bg-slate-950 border",
-                      "flex flex-col items-center justify-center min-h-64 p-6",
-                      "border-2 border-dashed rounded-lg border-slate-400 dark:border-slate-700",
-                      "cursor-pointer",
-                      "landscape:aspect-paper-A-horizontal portrait:aspect-paper-A-vertical"
-                    )}
-                    >
-                      <div className={clsx(
-                        "flex flex-col items-center justify-center pt-5 pb-6",
-                        "text-slate-400 dark:text-slate-500 ",
-                      )}>
-                        <PaperPlus width={64} height={64} />
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 font-semibold">
-                          {t('features.clientDatabase.fileLoader.dropzoneLabel')}
-                        </p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">
-                          {t('features.clientDatabase.fileLoader.supportedExtensions', {
-                            fileExtensions: '.asice'
-                          })}
-                        </p>
-                      </div>
-                      <input {...getInputProps()} />
-                    </label>
-                  </div>
-                ),
-                global?.document?.body
-              )
-            }
+            <Button {...getRootProps(p)} onPress={open} />
+            <input {...getInputProps()} />
           </>
+        )}
+      </Dropzone>
+      <Dropzone {...dropzoneProps}>
+        {({getRootProps, getInputProps, open}) => (
+          <Modal
+            isOpen={showDropzone.isEnabled}
+            onClose={showDropzone.unset}
+            backdrop="blur"
+          >
+            <ModalContent>
+              <ModalBody {...getRootProps({
+                className: clsx(
+                  "flex flex-col items-center justify-center pt-5 pb-6",
+                  "text-slate-400 dark:text-slate-500 ",
+                )
+              })}>
+                <PaperPlus width={64} height={64} />
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 font-semibold">
+                  {t('features.clientDatabase.fileLoader.dropzoneLabel')}
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  {t('features.clientDatabase.fileLoader.supportedExtensions', {
+                    fileExtensions: '.asice'
+                  })}
+                </p>
+                <input {...getInputProps()} />
+              </ModalBody>
+            </ModalContent>
+          </Modal>
         )}
       </Dropzone>
     </>
