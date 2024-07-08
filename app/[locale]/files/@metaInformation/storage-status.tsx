c41f188fce6fa70213b5ed5fc175b0b540a2dca6
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-07-08 22:01:39 +0200

    🏗️ This whole storage size thing is flaky in different browsers.

--- app/[locale]/files/@metaInformation/storage-status.tsx
+++ app/[locale]/files/@metaInformation/storage-status.tsx
@@ -5,10 +5,12 @@ import {PieChart, Pie, Cell, ResponsiveContainer, Legend} from 'recharts';
 
 export default (() => {
   if (typeof window === 'undefined') return null;
-  const estimate = use(navigator.storage.estimate());
+  let {usage, quota} = use(navigator.storage.estimate());
+  if (usage === undefined) usage = usage ?? 0;
+  if (quota === undefined) quota = quota ?? 0;
   const data: Data = [
-    {label: 'Quota', value: estimate.quota ?? 0},
-    {label: 'Usage', value: estimate.usage ?? 0},
+    {label: 'Free', value: quota - usage},
+    {label: 'Used', value: usage},
   ];
 
   if (!data) return null;
@@ -27,8 +29,14 @@ export default (() => {
           legendType='circle'
           label={(data) => byteFormatter.format(data.payload.value)}
         >
-          <Cell fill={'hsl(var(--nextui-primary)/var(--nextui-primary-opacity,var(--tw-bg-opacity)))'} />
-          <Cell fill={'hsl(var(--nextui-danger)/var(--nextui-danger-opacity,var(--tw-bg-opacity)))'} />
+          <Cell
+            fill={'hsl(var(--nextui-primary)/var(--nextui-primary-opacity,var(--tw-bg-opacity)))'}
+            stroke={'hsl(var(--nextui-content1)/var(--nextui-content1-opacity,var(--tw-bg-opacity)))'}
+          />
+          <Cell
+            fill={'hsl(var(--nextui-danger)/var(--nextui-danger-opacity,var(--tw-bg-opacity)))'}
+            stroke={'hsl(var(--nextui-content1)/var(--nextui-content1-opacity,var(--tw-bg-opacity)))'}
+          />
         </Pie>
       </PieChart>
     </ResponsiveContainer>
@@ -36,6 +44,6 @@ export default (() => {
 }) satisfies FunctionComponent;
 
 type Data = [
-  quota: {label: ReactNode; value: number},
-  usage: {label: ReactNode; value: number},
+  free: {label: ReactNode; value: number},
+  used: {label: ReactNode; value: number},
 ];
--- lib/byte-formatter.ts
+++ lib/byte-formatter.ts
@@ -1,6 +1,7 @@
 export function format(bytes: number, decimals: number = 2): string {
   if (bytes === 0) return '0 B';
-  const k = 1024;
+  // const k = 1024;
+  const k = Math.pow(10, 3);
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
